40cf5f0f6c56766629247779e404c48d29ab2f73
zoufanqi <zoufanqi@zhugeio.com>
2021-08-08 20:41:38 +0800

    迭代器模式

---
+++ java/src/main/java/designpattern/行为型模式/中介者模式/Mediator.java
@@ -0,0 +1,18 @@
+package designpattern.行为型模式.中介者模式;
+
+/**
+ * 抽象中介者
+ *
+ * @author: ZOUFANQI
+ * @create: 2021-08-08 20:18
+ **/
+public abstract class Mediator {
+    public abstract void register(Colleague colleague);
+
+    /**
+     * 转发
+     *
+     * @param colleague
+     */
+    public abstract void relay(Colleague colleague);
+}
