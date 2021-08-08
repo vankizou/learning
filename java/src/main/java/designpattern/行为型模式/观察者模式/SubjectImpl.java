c42f2c6e2091de848df688e84fba41143a813ce5
zoufanqi <zoufanqi@zhugeio.com>
2021-08-08 20:11:01 +0800

    观察者模式

---
+++ java/src/main/java/designpattern/行为型模式/观察者模式/SubjectImpl.java
@@ -0,0 +1,19 @@
+package designpattern.行为型模式.观察者模式;
+
+/**
+ * 具体目标
+ *
+ * @author: ZOUFANQI
+ * @create: 2021-08-08 19:58
+ **/
+public class SubjectImpl extends Subject {
+    @Override
+    public void notifyObserver() {
+        System.out.println("具体目标发生改变……");
+        System.out.println("--------------");
+
+        for (Observer o : observers) {
+            o.response();
+        }
+    }
+}
