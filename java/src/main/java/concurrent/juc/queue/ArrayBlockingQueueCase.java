61cf26c1f1770a2a6231cf8aa5519fe2a095e070
zoufanqi <zoufanqi@zhugeio.com>
2021-08-13 13:59:53 +0800

    queue

--- java/src/main/java/concurrent/juc/queue/ArrayBlockingQueueCase.java
+++ java/src/main/java/concurrent/juc/queue/ArrayBlockingQueueCase.java
@@ -3,6 +3,8 @@ package concurrent.juc.queue;
 import java.util.concurrent.ArrayBlockingQueue;
 
 /**
+ * 数组类型的阻塞队列
+ *
  * @author: ZOUFANQI
  * @create: 2021-08-11 14:04
  **/
@@ -15,7 +17,7 @@ public class ArrayBlockingQueueCase {
         queue.put(3);
         System.out.println(queue.take());
 
-        // 放置在idx=0上
+        // 队列总大小3，前面已经放满了（但拿走了第0索引的数据），所以会放置在idx=0上
         queue.put(4);
         System.out.println(queue.peek());
         System.out.println(queue.peek());
--- java/src/main/java/concurrent/juc/queue/SynchronousQueueCase.java
+++ java/src/main/java/concurrent/juc/queue/SynchronousQueueCase.java
@@ -3,6 +3,7 @@ package concurrent.juc.queue;
 import java.util.concurrent.SynchronousQueue;
 
 /**
+ * 不存储数据的阻塞队列
  * @author: ZOUFANQI
  * @create: 2021-08-11 14:37
  **/
@@ -18,13 +19,14 @@ public class SynchronousQueueCase {
         Thread.sleep(100);
         System.out.println(queue.offer("2"));   // true
         Thread.sleep(100);
-        System.out.println(queue.offer("3"));   // false
+        System.out.println(queue.offer("3"));   // false，已没有阻塞消费者（take()）
     }
 
     private static class UserThread extends Thread {
         @Override
         public void run() {
             try {
+                // 阻塞拿一次，退出
                 System.out.println(Thread.currentThread() + " - " + queue.take());
             } catch (InterruptedException e) {
                 e.printStackTrace();
