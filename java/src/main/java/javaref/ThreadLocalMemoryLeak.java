56ce6ad8c0d7593f39c26150390fd876fb8be476
zoufanqi <zoufanqi@zhugeio.com>
2021-08-16 11:31:00 +0800

    tl

--- java/src/main/java/javaref/ThreadLocalMemoryLeak.java
+++ java/src/main/java/javaref/ThreadLocalMemoryLeak.java
@@ -25,7 +25,7 @@ public class ThreadLocalMemoryLeak {
     public static void main(String[] args) {
         /**
          * 执行代码前先设置 vm options：
-         * -Xmx10M (应用程序内存在8M左右)
+         * -Xmx10M -XX:PrintGCDetails (应用程序内存在8M左右)
          */
 
         // step1
@@ -68,9 +68,31 @@ public class ThreadLocalMemoryLeak {
         // step2: +2M（第二次TL，强引用，正常）
         // step3: +2M（创建的字节数组）
         // step4: +1M（再次追加1M字节数组数据到内存，但内存已不足，触发gc）
-        // step4: -1M（此时触发minor gc，回收第一次创建的字节数组，注意：第一次的TL对象因为内存泄露没有被回收）
+        // step4: -1M（此时触发gc，回收第一次创建的字节数组，注意：第一次的TL对象因为内存泄露没有被回收）
         // step5: +2M（最后一次加2M数据到堆，但此时已无内存，抛出OOM）
 
+
+        // 打印：
+//        step1: [B@610455d6
+//        step2: [B@511d50c0
+//        step3: [B@511d50c0
+//                [GC (Allocation Failure) [PSYoungGen: 1425K->496K(2560K)] 7569K->6664K(9728K), 0.0009793 secs] [Times: user=0.00 sys=0.01, real=0.00 secs]
+//        step4: [B@511d50c0
+//                [GC (Allocation Failure) --[PSYoungGen: 1584K->1584K(2560K)] 7752K->7760K(9728K), 0.0012585 secs] [Times: user=0.01 sys=0.00, real=0.00 secs]
+//[Full GC (Ergonomics) [PSYoungGen: 1584K->0K(2560K)] [ParOldGen: 6176K->5497K(7168K)] 7760K->5497K(9728K), [Metaspace: 3077K->3077K(1056768K)], 0.0039508 secs] [Times: user=0.01 sys=0.00, real=0.00 secs]
+//[GC (Allocation Failure) [PSYoungGen: 0K->0K(2560K)] 5497K->5497K(9728K), 0.0009247 secs] [Times: user=0.00 sys=0.00, real=0.00 secs]
+//[Full GC (Allocation Failure) [PSYoungGen: 0K->0K(2560K)] [ParOldGen: 5497K->5479K(7168K)] 5497K->5479K(9728K), [Metaspace: 3077K->3077K(1056768K)], 0.0036259 secs] [Times: user=0.01 sys=0.00, real=0.01 secs]
+//        Heap
+//        PSYoungGen      total 2560K, used 135K [0x00000007bfd00000, 0x00000007c0000000, 0x00000007c0000000)
+//        eden space 2048K, 6% used [0x00000007bfd00000,0x00000007bfd21d58,0x00000007bff00000)
+//        from space 512K, 0% used [0x00000007bff80000,0x00000007bff80000,0x00000007c0000000)
+//        to   space 512K, 0% used [0x00000007bff00000,0x00000007bff00000,0x00000007bff80000)
+//        ParOldGen       total 7168K, used 5479K [0x00000007bf600000, 0x00000007bfd00000, 0x00000007bfd00000)
+//        object space 7168K, 76% used [0x00000007bf600000,0x00000007bfb59ce0,0x00000007bfd00000)
+//        Metaspace       used 3119K, capacity 4496K, committed 4864K, reserved 1056768K
+//        class space    used 347K, capacity 388K, committed 512K, reserved 1048576K
+//        Exception in thread "main" java.lang.OutOfMemoryError: Java heap space
+//        at javaref.ThreadLocalMemoryLeak.main(ThreadLocalMemoryLeak.java:63)
     }
 
 }
