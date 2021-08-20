ec063963e163184a097ae7491e13f06b6dc84bfd
zoufanqi <zoufanqi@zhugeio.com>
2021-08-20 19:19:36 +0800

    handler

--- java/src/main/java/nio/netty/NettyServerHandler.java
+++ java/src/main/java/nio/netty/NettyServerHandler.java
@@ -3,7 +3,6 @@ package nio.netty;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelFutureListener;
-import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.CharsetUtil;
